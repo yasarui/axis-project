1ee1cde2c3761798a0845cd173ea87be409cea78
Yasar Arafat <yasararafatui@gmail.com>
2019-11-05 07:28:39 +0530

    Added the Route

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React,{Fragment} from 'react';
 import './App.scss';
 import Header from './components/Header';
 import SideBar from './components/Sidebar';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Initiate from './components/Initiate';
+import Enquire from './components/Enquire';
+
 
 const App = () => {
    return(
-     <Fragment>
+     <Router>
         <div className="whole-wrapper">
             <Header />
             <div className="main-section">
               <SideBar />
               <div className="content-section">
+                 <Switch>
+                     <Route path="/" component={Dashboard} exact />
+                     <Route path="/initiate" component={Initiate} />
+                     <Route path="/enquire" component={Enquire} /> 
+                 </Switch>
               </div>
             </div>
         </div>
-     </Fragment>
+     </Router>
    )
 }
 
