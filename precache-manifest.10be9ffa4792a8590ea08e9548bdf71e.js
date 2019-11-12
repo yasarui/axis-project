6a9077813889640c1251671c08f77a4bbb64eec6
Yasar Arafat <yasararafatui@gmail.com>
2019-11-12 23:11:26 +0530

    Updates

---
+++ precache-manifest.10be9ffa4792a8590ea08e9548bdf71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fc4d77a58f81ac4f1a47ab3ba24d33",
+    "url": "/axis-project/index.html"
+  },
+  {
+    "revision": "8c4144b54a3da1719732",
+    "url": "/axis-project/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b9fe911b264e95bf2be2",
+    "url": "/axis-project/static/css/main.1ac3d57b.chunk.css"
+  },
+  {
+    "revision": "8c4144b54a3da1719732",
+    "url": "/axis-project/static/js/2.cc08ace0.chunk.js"
+  },
+  {
+    "revision": "b9fe911b264e95bf2be2",
+    "url": "/axis-project/static/js/main.9d417e5f.chunk.js"
+  },
+  {
+    "revision": "e95ac479d8f2a6c0a4bf",
+    "url": "/axis-project/static/js/runtime-main.550fd103.js"
+  }
+]);
