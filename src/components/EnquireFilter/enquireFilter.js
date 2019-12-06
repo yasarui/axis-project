ce4d071dfe0f751ba08ee0fdcfb7ce336e388fb8
Yasar Arafat <yasararafatui@gmail.com>
2019-12-06 08:25:11 +0530

    Date Picker added

--- src/components/EnquireFilter/enquireFilter.js
+++ src/components/EnquireFilter/enquireFilter.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import './enquireFilter.scss';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+import moment from 'moment';
+
+const CalenderIcon = () => {
+   return(
+    <i class="fas fa-calendar-alt"></i> 
+   )
+}
 
 class EnquireFilter extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      date: [new Date(), new Date()],
+    }
   }
+
+  onChange = date => this.setState({ date })
 
   render() {
     return (
       <div className="enquire-wrap clearfix">
         <div class="enquire-filter clearfix">
           <label>Date Range</label>
-          <div className="enquire-filters">
+          {/* <div className="enquire-filters">
             <input type="text" placeholder="dd-mm-yyyy" />
             <span>to</span>
             <input type="text" placeholder="dd-mm-yyyy" />
+          </div> */}
+          <div>
+             {/* <i class="fas fa-calendar-alt"></i> */}
+             <DateRangePicker
+              name="Yasar Arafat"
+              onChange={this.onChange}
+              value={this.state.date}
+              calendarIcon={<CalenderIcon />}
+           />
           </div>
-          <i class="fas fa-calendar-alt"></i>
         </div>
         <div class="enquire-filter">
           <label>Account Number</label>
