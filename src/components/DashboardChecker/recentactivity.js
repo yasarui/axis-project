81fb3748f4cfbb03a0b956c931eb2872c8bb0dde
Yasar Arafat <yasararafatui@gmail.com>
2019-12-09 08:08:25 +0530

    Added Sorting in Checkbox

--- src/components/DashboardChecker/recentactivity.js
+++ src/components/DashboardChecker/recentactivity.js
@@ -26,57 +26,120 @@ const createTd = (index,item,setModalIsOpen) => {
 
 const RecentActivity = ({data}) => {
 
+  const [recentData,setRecentData] = useState(data);
   const [modalIsOpen,setModalIsOpen] = useState(false);
+
+  const sortAssending = (e,key) => {
+    let temp = [...recentData];
+    temp.sort((a,b)=>{
+        if(a[key] < b[key]) return -1;
+        if(a[key] > b[key]) return 1;
+        return 0;
+    })
+    setRecentData(temp)
+ }
+
+  const sortDessending = (e,key) => {
+      let temp = [...recentData];
+      temp.sort((a,b)=>{
+          if(a[key] < b[key]) return 1;
+          if(a[key] > b[key]) return -1;
+          return 0;
+      })
+      setRecentData(temp)
+  }
+
+  const openViewDetailsModal = () => {
+    console.log("I am inside recent activity ")
+    setModalIsOpen(true);
+    console.log("Modal state is ",modalIsOpen);
+  }
 
    return (
       <div className="table-wrap recent-activity-table-wrapper">
        <table>
           <thead>
              <tr>
-               <th style={{"width":"119px"}} > Batch ID </th>
-               <th style={{"width":"194px"}}> Staff </th>
-               <th style={{"width":"161px"}}> Created </th>
-               <th style={{"width":"162px"}}> Accepted </th>
-               <th style={{"width":"162px"}}> Put on hold </th>
-               <th style={{"width":"162px"}}> Rejected </th>
-               <th style={{"width":"200px"}}> Last Activity date </th>
+               <th> Batch ID </th>
+               <th> 
+                  Staff 
+                  <div className="sortIconsWrapper">
+                     <span onClick={(e)=>sortAssending(e,'name')} className="headerSortUp"></span>
+                     <span onClick={(e)=>sortDessending(e,'name')} className="headerSortDown"></span>
+                   </div>
+               </th>
+               <th> 
+                   Created 
+                   <div className="sortIconsWrapper">
+                     <span onClick={(e)=>sortAssending(e,'created')} className="headerSortUp"></span>
+                     <span onClick={(e)=>sortDessending(e,'created')} className="headerSortDown"></span>
+                   </div>
+                </th>
+               <th> 
+                   Accepted 
+                   <div className="sortIconsWrapper">
+                     <span onClick={(e)=>sortAssending(e,'accepted')} className="headerSortUp"></span>
+                     <span onClick={(e)=>sortDessending(e,'accepted')} className="headerSortDown"></span>
+                   </div>
+               </th>
+               <th> 
+                    Put on hold 
+                    <div className="sortIconsWrapper">
+                     <span onClick={(e)=>sortAssending(e,'putOnHold')} className="headerSortUp"></span>
+                     <span onClick={(e)=>sortDessending(e,'putOnHold')} className="headerSortDown"></span>
+                   </div>
+               </th>
+               <th> 
+                   Rejected 
+                   <div className="sortIconsWrapper">
+                     <span onClick={(e)=>sortAssending(e,'rejected')} className="headerSortUp"></span>
+                     <span onClick={(e)=>sortDessending(e,'rejected')} className="headerSortDown"></span>
+                   </div>
+               </th>
+               <th> 
+                   Last Activity date 
+                   <div className="sortIconsWrapper">
+                     <span onClick={(e)=>sortAssending(e,'date')} className="headerSortUp"></span>
+                     <span onClick={(e)=>sortDessending(e,'date')} className="headerSortDown"></span>
+                   </div>
+               </th>
              </tr>
           </thead>
           <tbody>
-              {data.map((item,index)=>{
+              {recentData.map((item,index)=>{
                   return (
                     <tr>
-                      <td style={{"width":"104px"}} > 
+                      <td> 
                         <i class="icon fa fa-eye" aria-hidden="true"></i> {item._id.substr(1,5)}
                       </td>
-                      <td style={{"width":"210px"}} > 
+                      <td> 
                          {item.name} 
                       </td>
-                      <td style={{"width":"157px"}}> 
+                      <td> 
                         {createTd(index,item,setModalIsOpen)}
                       </td>
-                      <td style={{"width":"157px"}}> 
+                      <td> 
                          {(index < 3) ? <EmptyTd />: 
                          (<Fragment>
                             {item.accepted} <a className="view-details" href="#"> View Transactions
                            </a>
                          </Fragment>)}
                         </td>
-                      <td style={{"width":"157px"}}> 
+                      <td> 
                       {(index < 3) ? <EmptyTd /> : 
                          (<Fragment>
                             {item.putOnHold} <a className="view-details" href="#"> View Transactions
                            </a>
                          </Fragment>)}
                       </td>
-                      <td style={{"width":"157px"}} > 
+                      <td> 
                       {(index < 3) ? <EmptyTd /> : 
                          (<Fragment>
                             {item.rejected} <a className="view-details" href="#"> View Transactions
                            </a>
                          </Fragment>)} 
                       </td>
-                      <td style={{"width":"222px"}} > 
+                      <td> 
                          <span className="date" > {moment(item.date).format('dddd MM-DD-YY')} </span>
                          <span className="time"> {moment(item.date).format('h:mm:ss a')} </span>    
                       </td>
