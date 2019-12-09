d3f61dd5fd617b932781c8ca16aec6e20caf992b
Yasar Arafat <yasararafatui@gmail.com>
2019-12-09 08:49:15 +0530

    dynamic dropdown

--- src/components/ButtonDropdowns/button_dropdown.js
+++ src/components/ButtonDropdowns/button_dropdown.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './button_dropdown.scss';
 const ButtonDropdown = ({varient}) => {
+   const [value,setValue] = useState(varient);
+   function handleChange(e){
+       console.log("value ",e.target.value);
+   }
    return(
     <div className="axis-select-dropdown-wrapper">
-        <select className={varient} value={varient}>
+        <select onChange={(e)=>setValue(e.target.value)} className={value} value={value}>
             <option value="success"> Accept </option>
             <option value="warning" > Put on Hold </option>
             <option value="danger" > Reject </option>
