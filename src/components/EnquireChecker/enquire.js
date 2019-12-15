7e714268e8502ab60a61f100122be1f992291c18
Yasar Arafat <yasararafatui@gmail.com>
2019-12-15 18:41:52 +0530

    Route issue fix

--- src/components/EnquireChecker/enquire.js
+++ src/components/EnquireChecker/enquire.js
@@ -4,27 +4,35 @@ import EnquireFilter from '../EnquireFilter';
 // import EnquireTable from '../EnquireTable';
 import EnquireTable from '../EnquireTableNew';
 import RouteSelect from '../Utils/RouteSelect'
+import { useHistory } from "react-router-dom";
 
-class Enquire extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
+const Enquire = (props) => {
+  let history = useHistory();
+  const handleChange = (e) => {
+    history.push(`${e.target.value}`);
+  };
+  return(
+    <div>
         <div className="mobile-enq-checker">
-          <span  onClick={()=>this.props.setSelectedIndex(0)}><i class="fas fa-chevron-left"></i> Back</span>
+          <span  onClick={()=>props.setSelectedIndex(0)}><i class="fas fa-chevron-left"></i> Back</span>
           <h1 className="page-title">Transaction</h1>
         </div>
         <div className="payment-header">
-           <RouteSelect />
+          <div className="route-select">
+              <span> Viewing as </span>
+                <select className="web-route" onChange={handleChange}>
+                    <option value="/checker"> Checker </option>
+                    <option value="/"> Maker </option>
+                </select>
+                <select className="mobile-route" onChange={handleChange}>
+                    <option value="/"> Checker </option>
+                </select>
+          </div>
         </div>
         <EnquireFilter />
         <EnquireTable />
-      </div>
-    )
-  }
+     </div>    
+  )
 }
 
 export default Enquire;
